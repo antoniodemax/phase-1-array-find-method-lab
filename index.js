89dfe1af5a1b9f6bf69f782d93a410f79d2d0d0e
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-10 23:44:49 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code your solution here
+
+// Write the function
 superbowlWin = (record) => {
     const result = record.find(record => record.result === "W");
+    //return the year
     return !!result ? result.year :undefined;
 }
 
